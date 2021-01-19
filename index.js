ffea1ff97fcf1c88af4e8e05b88ff7ee02110256
Alex <alex.gyumushyan@gmail.com>
2021-01-19 09:54:20 -0500

    added functionality to display the current price of a specific stock and refresh it every second

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const api_url = 'https://cloud.iexapis.com/stable/stock/amzn/batch?types=quote,news,chart&range=1m&last=10&token=pk_6591e91f712e4ab7b7e1eac1af451489'
+const api_url = 'https://cloud.iexapis.com/stable/stock/tsla/batch?types=quote,news,chart&range=1m&last=10&token=pk_6591e91f712e4ab7b7e1eac1af451489'
              
 async function getData() {
     const response = await fetch (api_url);
@@ -7,10 +7,19 @@ async function getData() {
     console.log(data.quote.latestTime);
     console.log(data.quote.peRatio);
 
-    let currentPrice = data.quote.close;
+    let currentPrice = data.quote.latestPrice;
+
+
+    //change will occur here too 
+    document.getElementById("price").innerHTML = currentPrice.toString();
 
 }
 
 getData();
 
+var myVar = setInterval(refresh, 1000);
+
+function refresh(){
+    getData();
+}
 
