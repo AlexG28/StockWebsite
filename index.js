a8c5e934f26b56d3f5791f5e7d8bcbc0560e7cb3
Alex <alex.gyumushyan@gmail.com>
2021-01-19 23:06:36 -0500

    reworked js file to work based on a collection of tickers. Having issues with promsie

--- index.js
+++ index.js
@@ -1,25 +1,55 @@
-const api_url = 'https://cloud.iexapis.com/stable/stock/tsla/batch?types=quote,news,chart&range=1m&last=10&token=pk_6591e91f712e4ab7b7e1eac1af451489'
-             
-async function getData() {
+// list of tickers is 
+//var banks = ['jpm', 'bac', 'wfc', 'c', 'gs']
+var banks = ['tsla', 'jpm']
+
+
+async function getData(ticker) {
+    api_url = 'https://cloud.iexapis.com/stable/stock/' + ticker + '/batch?types=quote&token=pk_6591e91f712e4ab7b7e1eac1af451489';
+    
+
     const response = await fetch (api_url);
     const data = await response.json();
-    console.log(data);
-    console.log(data.quote.latestTime);
-    console.log(data.quote.peRatio);
-
-    let currentPrice = data.quote.latestPrice;
+    //console.log(data);
+    var price = (data.quote.latestPrice).toString();
 
 
-    //change will occur here too 
-    document.getElementById("price").innerHTML = currentPrice.toString();
+    let myPromise = new Promise(function(myResolve, myReject) {
+        
+        if (price != '') {
+          myResolve("OK");
+        } else {
+          myReject("Error");
+        }
+    });
+      
+    myPromise.then(
+        function(value) {return price;},
+        function(error) {alert('It didnt work');}
+    );
 
+
+    
 }
 
-getData();
+refresh();
 
-var myVar = setInterval(refresh, 1000);
+//var myVar = setInterval(refresh, 5000);
 
 function refresh(){
-    getData();
+    var text1 = updateList(banks);
+    document.getElementById('price').innerHTML = text1;
+    //getData('tsla');
+}
+
+
+function updateList(tickers) {
+
+    var outputString = ""
+
+    for (i = 0; i < tickers.length; i++) {
+        outputString = outputString + getData(tickers[i]) + '\n';
+    }
+    console.log(outputString);
+    return outputString;
 }
 
