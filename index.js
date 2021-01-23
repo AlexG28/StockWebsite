2b274603d186eb09df65de42244e5b0bfb2c1fc2
Alex <alex.gyumushyan@gmail.com>
2021-01-22 22:00:27 -0500

    fixed table and added come visual customizations for it

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // list of tickers is 
-var banks = ['jpm', 'bac', 'wfc', 'c', 'gs']
-
+//var banks = ['jpm', 'bac', 'wfc', 'c', 'gs']
+var banks = {'name': 'Banks', 'symbols': ['jpm', 'bac', 'wfc', 'c', 'gs']}
 
 
 async function getData(ticker) {
@@ -8,6 +8,7 @@ async function getData(ticker) {
     
     const response = await fetch (api_url);
     const data = await response.json();
+    console.log(data);
     var price = (data.quote.latestPrice).toFixed(2).toString();
     return price;
 }
@@ -17,24 +18,31 @@ refresh();
 //var myVar = setInterval(refresh, 5000); refresh every 5 seconds 
 
 async function refresh(){
-    var output = await updateList(banks);
-    document.getElementById('price').innerHTML = output;
+    var output = await updateList(banks['symbols']);
+    //document.getElementById('price').innerHTML = output;
+    
+    // add the data into the table
+    const tableBody = document.getElementById('tableData');
+    let dataHtml = '';
+
+    for (i = 0; i < output.length; i++) {
+        dataHtml += `<tr> <td>${output[i].ticker}</td>  <td>${output[i].price}</td> </tr>`;
+    }
+    tableBody.innerHTML = dataHtml;
 }
 
 
 async function updateList(tickers) {
 
-    var outputString = ""
+    var outputArray = [];
 
     for (i = 0; i < tickers.length; i++) {
         price = await getData(tickers[i]);
-        // promise <state> = 'pending'
-        
-        if (price != 'error'){
-            outputString = outputString + price + '\n';
-        }
+            
+        temparray = {'ticker' : tickers[i] , 'price' : price};
+        outputArray.push(temparray);
     }
     
-    return outputString;
+    return outputArray;
 }
 
